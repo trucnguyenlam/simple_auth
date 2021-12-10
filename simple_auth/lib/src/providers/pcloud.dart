816f76350d793790af8f01a24c75c076268f17ae
Truc Nguyen Lam <trucnguyenlam@gmail.com>
2021-12-10 21:37:16 +0000

    improve pcloud, use force_reapprove

--- simple_auth/lib/src/providers/pcloud.dart
+++ simple_auth/lib/src/providers/pcloud.dart
@@ -1,21 +1,15 @@
 import 'dart:async';
+import 'dart:convert';
+import 'dart:math';
 
-import 'package:simple_auth/simple_auth.dart';
 import "package:http/http.dart" as http;
+import 'package:simple_auth/simple_auth.dart';
 
 class PCloudApi extends OAuthApi {
-  PCloudApi(String identifier, String clientId, String clientSecret,
-      String redirectUrl,
-      {List<String>? scopes,
-        http.Client? client,
-        Converter? converter,
-        AuthStorage? authStorage,
-        bool useEmbeddedBrowser = false})
+  PCloudApi(String identifier, String clientId, String clientSecret, String redirectUrl,
+      {List<String>? scopes, http.Client? client, Converter? converter, AuthStorage? authStorage, bool useEmbeddedBrowser = false})
       : super.fromIdAndSecret(identifier, clientId, clientSecret,
-      client: client,
-      scopes: scopes,
-      converter: converter,
-      authStorage: authStorage) {
+            client: client, scopes: scopes, converter: converter, authStorage: authStorage) {
     this.scopesRequired = false;
     this.tokenUrl = "https://api.pcloud.com/oauth2_token";
     this.baseUrl = "https://api.pcloud.com";
@@ -24,12 +18,11 @@ class PCloudApi extends OAuthApi {
     this.useEmbeddedBrowser = useEmbeddedBrowser;
   }
 
-  Authenticator getAuthenticator() => PCloudAuthenticator(identifier, clientId,
-      clientSecret, tokenUrl, authorizationUrl, redirectUrl!, scopes, useEmbeddedBrowser);
+  Authenticator getAuthenticator() =>
+      PCloudAuthenticator(identifier, clientId, clientSecret, tokenUrl, authorizationUrl, redirectUrl!, scopes, useEmbeddedBrowser);
 
   @override
-  Future<OAuthAccount> getAccountFromAuthCode(
-      WebAuthenticator authenticator) async {
+  Future<OAuthAccount> getAccountFromAuthCode(WebAuthenticator authenticator) async {
     var auth = authenticator as PCloudAuthenticator;
     return OAuthAccount(identifier,
         created: DateTime.now().toUtc(),
@@ -43,18 +36,21 @@ class PCloudApi extends OAuthApi {
 
 class PCloudAuthenticator extends OAuthAuthenticator {
   late Uri redirectUri;
-  PCloudAuthenticator(String? identifier, String? clientId, String? clientSecret,
-      String? tokenUrl, String? baseUrl, String redirectUrl, List<String>? scopes, bool useEmbeddedBrowser)
-      : super(identifier, clientId, clientSecret, tokenUrl, baseUrl,
-      redirectUrl) {
+  late String state;
+
+  PCloudAuthenticator(String? identifier, String? clientId, String? clientSecret, String? tokenUrl, String? baseUrl,
+      String redirectUrl, List<String>? scopes, bool useEmbeddedBrowser)
+      : super(identifier, clientId, clientSecret, tokenUrl, baseUrl, redirectUrl) {
     this.useEmbeddedBrowser = useEmbeddedBrowser;
     authCodeKey = "access_token";
+    state = _createCryptoRandomString();
     redirectUri = Uri.parse(redirectUrl);
   }
+
   String? token;
   String? tokenType;
-  String? state;
   String? uid;
+
   bool checkUrl(Uri url) {
     try {
       /*
@@ -66,15 +62,17 @@ class PCloudAuthenticator extends OAuthAuthenticator {
         url = url.replace(query: url.fragment);
       }
 
-      if (url.host != redirectUri.host) return false;
+      if (!url.toString().contains(redirectUri.toString())) return false;
       if (url.query.isEmpty) return false;
+
+      // verify state
+      if (!url.queryParameters.containsKey('state') || url.queryParameters['state'] != state) return false;
+
       if (!url.queryParameters.containsKey(authCodeKey)) return false;
       var code = url.queryParameters[authCodeKey];
       if (code?.isEmpty ?? true) return false;
       token = code;
-      tokenType = url.queryParameters["token_type"] == 'bearer'
-          ? 'Bearer'
-          : url.queryParameters["token_type"];
+      tokenType = url.queryParameters["token_type"] == 'bearer' ? 'Bearer' : url.queryParameters["token_type"];
       uid = url.queryParameters["uid"];
       foundAuthCode(code);
       return true;
@@ -90,11 +88,26 @@ class PCloudAuthenticator extends OAuthAuthenticator {
       "client_id": clientId,
       "response_type": "token",
       "redirect_uri": redirectUrl,
+      "force_reapprove": "true",
     };
 
-    if (state?.isNotEmpty ?? false) {
+    if (state.isNotEmpty) {
       data["state"] = state;
     }
     return data;
+  }
+
+  String _createCryptoRandomString([int length = 32]) {
+    final random = _getRandom();
+    final values = List<int>.generate(length, (i) => random.nextInt(256));
+    return base64Url.encode(values);
+  }
+
+  Random _getRandom() {
+    try {
+      return Random.secure();
+    } catch (error, _) {
+      return Random(DateTime.now().millisecondsSinceEpoch);
+    }
   }
 }
